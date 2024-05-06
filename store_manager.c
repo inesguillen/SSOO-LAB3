97bab98fc209dbb70a0badde2565499fcbcad337
Golden <salvadorayalaxd@gmail.com>
2024-05-06 18:09:42 +0200

    Can read from file

--- store_manager.c
+++ store_manager.c
@@ -15,9 +15,44 @@
 
 int main (int argc, const char * argv[])
 {
+  // Check the number of arguments
+  if (argc != 5) {
+    printf("Usage: %s <file> <num_producers> <num_consumers> <queue_size>\n", argv[0]);
+    exit(-1);
+  }
+
   int profits = 0;
   int product_stock [5] = {0};
+  int values_table[5][2] = {{2, 3}, {5, 10}, {15, 20}, {25, 40}, {100, 125}}; // Table of purchase/sell prices
 
+  // Open the file
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL)
+  {
+    perror("Failed to open the file.");
+    exit(-1);
+  }
+
+  // Read the number of lines and allocate memory accordingly
+  int numLines;
+  fscanf(file, "%d", &numLines);
+  struct element *data = (struct element *)malloc(numLines * sizeof(struct element));
+
+  // Load the data into the struct
+  for (int i = 0; i < numLines; i++) {
+    char word1[100], word2[100], word3[100];
+    fscanf(file, "%s %s %s", word1, word2, word3);
+    data[i].product_id = atoi(word1);
+    if (strcmp(word2, "PURCHASE") == 0) {
+      data[i].op = 0;
+    } else {
+      data[i].op = 1;
+    }
+    data[i].units = atoi(word3);
+  }
+
+  // Close the file
+  fclose(file);
 
 
 
